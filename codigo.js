66eee3d0cc2fffc445410c208f38ec86882446aa
Moraschetti <moraschettimatias91@gmail.com>
2022-07-30 15:35:55 -0300

    15.35

--- codigo.js
+++ codigo.js
@@ -1,1 +1,11 @@
 alert("hola");
+
+"use strict";
+console.log("declarando funciones");
+let contador = 0;
+function clickear() {
+//incrementa el valor de contador
+contador++;
+console.log("Hiciste " + contador + " clicks")
+//es lo mismo que contador = contador + 1
+}
